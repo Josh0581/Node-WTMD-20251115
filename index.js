d8ea974969c316cb68aab936b44812eeb44a1596
josh <sh.jo@lgcns.com>
2025-11-15 12:35:18 +0900

    update index.js for api post /posts

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import posts from './mock.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 // 헬스 체크
 app.get('/', (req, res) => {
   res.send('Prisma + PostgreSQL API');
@@ -11,6 +13,19 @@ app.get('/', (req, res) => {
 
 app.get('/posts', (req, res) => {
   res.json(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // const { title, content } = req.body;
+  const newPost = {
+    id: posts.length ? posts[posts.length - 1].id + 1 : 1,
+    title,
+    content,
+  };
+  posts.push(newPost);
+  res.status(201).json(newPost);
 });
 
 // 서버 시작
